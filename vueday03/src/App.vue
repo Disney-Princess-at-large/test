060c435f15125408930a83aff63057615657995a
Li <1627843848@qq.com>
2022-06-30 20:11:18 +0800

    完成了加加减减案例

--- vueday03/src/App.vue
+++ vueday03/src/App.vue
@@ -1,8 +1,13 @@
+<!-- 目标: 点击生成按钮, 新增一个li(随机数字)和删除按钮, 点击删除按钮, 删除对应的li和值
+提示: 数组渲染列表, 生成和删除都围绕数组操作 -->
+
 <template>
   <div>
     <ul>
-      <li v-for="item in myArr" :key="item">{{ item }}</li>
-      <button @click="btn">点我前进</button>
+      <li v-for="(item, index) in arr" :key="index">
+      {{ item }}
+      <button @click="del(index)">删除</button></li>
+      <button @click="addFn">生成</button>
     </ul>
   </div>
 </template>
@@ -11,13 +16,16 @@
 export default {
   data() {
     return {
-      myArr: ["帅哥", "美女", "程序猿"],
+      arr: [1,5,3],
     }
   },
   methods: {
-    btn(){
-      this.myArr.push(this.myArr[0])
-      this.myArr.shift()
+    addFn(){
+      let num = Math.floor(Math.random() * 9)
+      this.arr.push(num)
+    },
+    del(index){
+      this.arr.splice(index, 1)
     }
   },
 }
