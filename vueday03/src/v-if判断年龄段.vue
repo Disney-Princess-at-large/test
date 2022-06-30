52a2023426b484e62ca4774b04c413a3a7ac8db2
Li <1627843848@qq.com>
2022-06-30 19:35:47 +0800

    完成了动态class导航

---
+++ vueday03/src/v-if判断年龄段.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-if="age >= 18">成年人</li>
+      <li v-else-if="age < 18 && age >= 12">青少年</li>
+      <li v-else>小孩</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      age:20
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueday03/src/动态class1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+
+    <p v-bind:class="{ active: activeFlag }">我是p标签</p>
+    <button @click="activeFlag = !activeFlag">Click me</button>
+    <!-- v-bind:class指令可以与普通的class特性共存 -->
+    <div class="box" :class="{ active: activeFlag }">我是div</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeFlag: true,
+    }
+  },
+}
+</script>
+
+<style></style>
