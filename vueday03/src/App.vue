2797705cead54f8a5d9edaba6c46c69b9b2f2cef
Li <1627843848@qq.com>
2022-06-30 19:24:56 +0800

    完成了v-if 判断年龄段的案例

--- vueday03/src/App.vue
+++ vueday03/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <p :class="{ red: true }">asdasd</p>
+    <ul>
+      <li v-if="age >= 18">成年人</li>
+      <li v-else-if="age < 18 && age >= 12">青少年</li>
+      <li v-else>小孩</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      age:20
+    }
+  },
+}
 </script>
 
 <style>
-.red {
-  color: red;
-}
+
 </style>
